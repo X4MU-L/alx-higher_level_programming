8c5950886dc3a1a6bcc6e37b79161d30544c384f
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-13 01:39:07 -0700

    (feat) -> print a list of integers in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Print a list of integers one line at a time in reverse"""
-    my_list = my_list[::-1]
-    if len(my_list) == 0:
-        print()
-        return
-    for i in my_list:
-        print("{:d}".format(i))
+    if my_list:
+        my_list = my_list[::-1]
+        for i in my_list:
+            print("{:d}".format(i))
