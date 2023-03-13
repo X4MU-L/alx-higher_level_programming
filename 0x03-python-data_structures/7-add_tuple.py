8e58578bf5211b63e7f7c7d6cfea2b81b4d86b1e
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-13 07:11:14 -0700

    (feat) -> add the 1st 2 element of a 2 tuples or replace with 0 if not available

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """add the first two elements of 2 different tuples differently
-    if any of the tuples first 2 elements are empty replace with 0"""
-    tuple_a += (0,0)
-    tuple_b += (0,0)
+    if any of the tuples first 2 elements are empty replace with 0
+    """
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
     new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return new_tuple
