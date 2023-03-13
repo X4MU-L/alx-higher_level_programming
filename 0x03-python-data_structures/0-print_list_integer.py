efae69e7e45662bc19442f0fdec7a27a176eec79
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-13 00:56:57 -0700

    (feat) -> print a list of integers one line at a time

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print a list of integers one line at a time"""
+    for i in my_list:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+#  Python - Data Structures: Lists, Tuples
+
+The projects in the folder helps the student to understand Python Data Structures.
