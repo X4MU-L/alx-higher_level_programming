f52500fb640e96e7fc74a0e1d88c802ba3de0051
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-13 01:51:06 -0700

    (feat) -> remove c or C from a string and return new string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """Print a list of integers one line at a time in reverse"""
+    if my_string:
+        new_str = ""
+        for c in my_string:
+            if c not in "cC":
+                new_str += c
+        return new_str
