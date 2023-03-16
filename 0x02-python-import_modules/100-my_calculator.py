8a142f7a1cbe3e55d29e83230bfd6be13e91ddc7
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-16 04:45:56 -0700

    (refactor) -> handle case of not input to function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     operators = ["*","+","/","-"]
-    functions = [mul,add,div,sub]
+    functions = [mul, add, div, sub]
     arg_len = len(sys.argv) - 1
-    operator = sys.argv[2]
 
     if arg_len < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -18,7 +17,7 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     try:
         idx = operators.index(operator)
-        print(f'{a} {operator} {b} = {functions[idx](a, b)}')
+        print(f'{a} {sys.argv[2]} {b} = {functions[idx](a, b)}')
     except:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
