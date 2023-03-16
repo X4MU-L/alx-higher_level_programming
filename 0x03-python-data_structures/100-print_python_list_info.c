ec26a5fbd0f98e5e7517a7b016169d44b15819cd
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-16 03:45:27 -0700

    (feat) -> print byton objects

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+
+	printf("[*] Allocated = %li\n", obj->allocated);
+
+	for (i = 0; i < size; i++)
+
+		printf("Element %i: %s\n", i, PyList_GET_ITEM(obj, i)->tp_name);
+
+}
