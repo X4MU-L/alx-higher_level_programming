351bb55918ec24d40c06476107f62906b5f30c47
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-17 18:16:53 -0700

    (feat) -> print the key:value of a dict sorted by the keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     keys = list(dict.keys(a_dictionary))
     for key in sorted(keys):
-        print(f"{key} : {a_dictionary[key]}")
+        print(f"{key}: {a_dictionary[key]}")
