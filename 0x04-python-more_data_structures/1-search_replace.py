8f7d6ea0f8778a918a01bc55876ae022f3db4022
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-17 17:26:36 -0700

    (feat) -> find and replace values in a list using list comprehention

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     if my_list:
-        my_list = [replace if x == search else x for x in my_list]
+        my_list = [replace if replace and x == search else x for x in my_list]
         return my_list
