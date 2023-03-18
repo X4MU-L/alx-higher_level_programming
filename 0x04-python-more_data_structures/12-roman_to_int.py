68d992a441bdde20092b2548ecb9e415b7684652
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-17 19:54:14 -0700

    (feat) -> convert a roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,13 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str) and roman_string:
         roman = {"I": 1, "V": 5, "X": 10, "L": 50,
                  "C": 100, "D": 500, "M": 1000}
-        tens = ["X", "V", "D", "C", "L", "M"]
+        units = ["X", "V", "D", "C", "L", "M"]
+        tens = ["L", "C", "M"]
         for i in range(len(roman_string)):
-            if roman_string[i] in tens and roman_string[i - 1] == "I":
+            if roman_string[i] in units and roman_string[i - 1] == "I":
                 num -= 2
+            if roman_string[i] in tens and roman_string[i - 1] == "X":
+                num -= 10
             num += roman[roman_string[i]]
         return (num)
     return (num)
