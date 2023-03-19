cf4019a53b4499aa35887f2b7ad553c39330e907
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-19 00:34:15 -0700

    (feat) -> print the possible different combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for k in range(1 + i, 10):
-        print("{}{}".format(i,k), end=", " if i != 8 else "\n")
+        print("{}{}".format(i, k), end=", " if i != 8 else "\n")
