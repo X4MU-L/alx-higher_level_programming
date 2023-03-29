dfcbb825b837cddfdc70d98610fbe18c0bf4d82a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-28 20:41:10 -0700

    (feat) -> create a class square with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This Module contains a python class Square"""
+class Square:
+    """Square is a python class with a private size attribute"""
+    def __init__(self, size):
+        """Initializes the square objects
+        called on class instanciation"""
+        self.__size = size
+
+    def __str__(self):
+        """This function is called by the Print function
+        Returns a string representation of the private attribute
+        size"""
+        return f"{{'_Square__size': {self.size}}}"
