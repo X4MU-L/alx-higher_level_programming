6e524c8fadeb6e3218420ff461243f60a5b025e3
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-03-28 21:09:18 -0700

    (feat) -> add class method to get the area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""This Module contains a python class Square"""
+class Square:
+    """Square is a python class with a private size attribute"""
+
+    def __init__(self, size=0):
+        """Initializes the square objects
+        called on class instanciation
+        Args:
+            size (int): the size of the new square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def __str__(self):
+        """This function is called by the Print function
+        Returns a string representation of the private attribute
+        size"""
+
+        return f"{{'_Square__size': {self.size}}}"
+
+    def area(self):
+        """Gets and returns the area of the square object"""
+
+        return self.__size**2
