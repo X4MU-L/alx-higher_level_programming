3c7d3ad060deebe23c12ff15a85048ac0f565b71
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-03 16:20:08 -0700

    (feat) -> Create an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Create an empty Rectangle class"""
+
+
+class Rectangle:
+    """This is an empty rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+#   Python - More Classes and Objects
+
+The projects in the folder helps the student to understand Python Classes and Objects
+
