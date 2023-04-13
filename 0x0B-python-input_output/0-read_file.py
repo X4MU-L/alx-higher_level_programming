91bfef412cb749654c91ff8955b7f4f82fbbb610
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-13 16:17:24 -0700

    (feat) -> read the contect of seeing that bieo ooo

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Read File Module"""
+
+def read_file(filename=""):
+    """Read and prints all the content of file
+       args:
+           filename - (strings) the name of the file to open
+    """
+    with open(filename, "r") as f:
+        print(f.read(), end="")
