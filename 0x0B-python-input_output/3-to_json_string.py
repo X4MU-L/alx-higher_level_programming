3e113431dabee95fa141c22672932a4d1d63c5ab
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-14 08:47:43 -0700

    (refactor) -> add function docstrings

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,4 +4,8 @@ import json
 
 
 def to_json_string(my_obj):
+    """pasrse a Json serializable to a string
+       Args:
+           my_obj: json object to parse
+    """
     return (json.dumps(my_obj))
