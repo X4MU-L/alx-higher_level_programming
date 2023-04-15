cebd1d72e7acc0e61ac4189de36c8a5915766d6d
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-15 01:27:35 -0700

    (feat) -> update a student class object from a json object

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Student class Module"""
+
+
+class Student:
+    """Defines a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instanciates a new Student object
+        Args:
+            first_name: (string) first name of the student.
+            last_name: (string) last name of the student.
+            age: (integer) age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a json/dict repsentation of an object"""
+        if (type(attrs) == list and all(type(item) == str
+                                        for item in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Update a Student Object from a json object"""
+        if type(json) == dict:
+            for key in json:
+                if hasattr(self, key):
+                    setattr(self, key, json[key])
