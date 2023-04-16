abbae10d0a588524e06faa5077099c6dd161ce0d
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-16 14:00:14 -0700

    (feat) -> create a __str__ method for Square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square class"""
+
+    def __init__(self, size):
+        """Initializes a Square object
+           args:
+               size: (int) width and height of the Square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(self.__class__.__name__, self.__size,
+                                    self.__size))
