c412137bf5f26f282bd47304a14bdda8d78ad793
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-16 10:26:33 -0700

    (feat) -> add area() method to BaseGeometry -: raises Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a Geometry class"""
+
+
+class BaseGeometry:
+    """Define a BaseGeometry class"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
