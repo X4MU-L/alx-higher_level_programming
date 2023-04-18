639881ec809373b84c083042cb3a469aa9d8cade
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-17 18:59:43 -0700

    (fix) -> name typo on variable

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,7 +37,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
@@ -56,3 +56,12 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
+
+r1 = Rectangle(10, 2)
+print(r1.id)
+
+r2 = Rectangle(2, 10)
+print(r2.id)
+
+r3 = Rectangle(10, 2, 0, 0, 12)
+print(r3.id)
