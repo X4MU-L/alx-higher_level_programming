3781c075a706e72f381fb7ca108719acfa31d893
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-17 18:29:24 -0700

    create a Rectangle class with getter/setter of private instance attributes

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Create a Rectangle class inherit from Base"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Define a Rectangle Class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a Rectangle Object
+           args:
+               width: (int) the width of the Rectangle
+               height: (int) the height of the Rectangle
+               y: (int) y attribute
+               x: (int) x attribute
+               id: (any) the id of the Rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get/set the width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/Set the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = height
+
+    @property
+    def x(self):
+        """Get/Set the x attribute"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get/Set the y attribute"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
