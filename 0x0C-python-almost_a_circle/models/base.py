28c50b08a873b03fde1c5989ac3bc85317bc901a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-19 10:45:21 -0700

    (feat) -> create a to_json_strings mothod in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Create a Base Module"""
+import json
 
 
 class Base:
@@ -18,3 +19,12 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """return a string representaion of a list of dicts
+        args:
+            list_dictionaries: a list of dict
+        """
+        if list_dictionaries is None or type(list_dictionaries) != list:
+            return "[]"
+        return json.dumps(list_dictionaries)
