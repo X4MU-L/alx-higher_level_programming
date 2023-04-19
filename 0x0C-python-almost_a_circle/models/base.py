352046ea68a1f0ee900a66de440151879707d43a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-19 10:50:59 -0700

    (feat) -> create a to_json_strings mothod in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,6 @@ class Base:
         args:
             list_dictionaries: a list of dict
         """
-        if list_dictionaries is None or type(list_dictionaries) != list:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
