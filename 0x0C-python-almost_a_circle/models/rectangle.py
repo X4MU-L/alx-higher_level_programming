b0c164e79cd102aaa9fa97c85bba0a1699326e3b
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-19 10:26:50 -0700

    (feat) -> create a to_dictionary method that returns a dict representation of Rectangle object

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,3 +135,13 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Return a dictionary representaion of Square object"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
