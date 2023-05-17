3c85be89a75fa4c9d2690a923981789410afd4be
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-05-17 18:47:42 +0100

    (feat) -> create a user and grant all privilages

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- create user_0d_1 and grand all privileges and set password to user_0d_1_pwd
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.*
