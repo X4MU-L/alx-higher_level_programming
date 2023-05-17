9b9473a99e7e6b0ee94f79455fa7841ad0de0b2a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-05-17 19:40:14 +0100

    (feat) -> create a table and a foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- create database hbtn_0d_usa and create table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+       id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+       state_id INT NOT NULL FOREIGN KEY REFRENCES states(id),
+       name VARCHAR(256) NOT NULL
+       );
