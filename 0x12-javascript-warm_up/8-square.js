4e06bc0a7bf0d4e5ed9c67e81724f425d7bcb0c2
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-14 14:39:49 +0100

    (feat) -> print square x

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints x times “C is fun" where x is from line args
+
+const arg = process.argv.slice(2, 3).join();
+
+if (!arg || Number.isNaN(Number(arg))) {
+  console.log('Missing size');
+} else {
+  for (let i = Number(parseInt(arg)); i > 0; i--) {
+    let x = '';
+    for (let j = Number(parseInt(arg)); j > 0; j--) {
+      x += 'x';
+    }
+    console.log(x);
+  }
+}
