dc230320baeecc6b70cb439144c275874cf7ae22
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-15 02:42:01 +0100

    (feat) -> rotate and double width and height

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// create a rectangle class object
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
