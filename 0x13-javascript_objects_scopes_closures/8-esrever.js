f9e68e9154b28da5e3157a3f0030c8ea68c5af2f
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-15 03:30:48 +0100

    (feat) -> reverse a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // create a fuction that reverses a list without reverse
 
-
 exports.esrever = function (list) {
-    let l = list.length;
-    return (list.map((item) => list[--l]));
-}
+  let l = list.length;
+  return (list.map((item) => list[--l]));
+};
