14c3c0d658a6f59050c3121bc24182616543c618
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-15 02:29:28 +0100

    (feat) -> print the rectangle using X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// create a rectangle class object
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
