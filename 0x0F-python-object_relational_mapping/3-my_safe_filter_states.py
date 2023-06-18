fefbb525d0f3725f1dcf757290e077f2cf58782f
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-19 00:20:45 +0100

    (feat) -> safe parentesized argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""print a tuple of all states in hbtn_0e_0_usa if any"""
+"""print a tuple of a state passed in the command in hbtn_0e_0_usa if any"""
 import MySQLdb
 from sys import argv
 
@@ -11,14 +11,15 @@ if __name__ == "__main__":
                              passwd=argv[2],
                              db=argv[3]
                              )
+        state_name = argv[4]
     except Exception as e:
         if type(e) == IndexError:
-            print(f"USAGE: {argv[0]} user passwd database")
-
-        print(e)
+            print(f"USAGE: {argv[0]} user passwd database state")
+        else:
+            print(e)
         exit(1)
     cur = db.cursor()
-    cur.execute("SELECT * from states WHERE name LIKE 'N%' \
-    ORDER BY id;")
+    cur.execute("SELECT * from states WHERE name=(%s) ORDER BY id;",
+                (state_name, ))
     [print(row) for row in cur.fetchall()]
     db.close()
