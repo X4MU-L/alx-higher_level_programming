c807ae8b0a4d11a8edc063173ce1ec431de32c52
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-03 02:23:54 +0100

    (feat) -> send a http request in a bash script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
-
-if [ $# -lt 1 ]
-then
-    echo "Usage: ${0:2} URL:[port]"
-    exit 1
-fi
-
 curl "$1"
