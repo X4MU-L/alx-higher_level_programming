769f0894e5b7161632c64a44b51b3d238c93826c
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-03 04:02:57 +0100

    (feat) -> get the peak of a number

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -20,11 +20,3 @@ def find_peak(arr):
             end = mid
 
     return arr[start]
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
