233818a418d5845b9e9e72d05f6e3ed0d8987c68
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-07 13:25:06 +0100

    (feat) -> use direct api call

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,11 +10,11 @@ from sys import argv
 
 if __name__ == "__main__":
     repo, username = argv[1:]
-    url = "https://api.github.com/search/commits"
-    url += f'?q=repo:{username}/{repo}&per_page=10&sort=committer-date'
-    r = requests.get(url)
+    url = f"https://api.github.com/repos/{username}/{repo}/commits?per_page=10&sort=committer-date"
+    r = requests.get(url, auth=(
+        username, "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"))
     try:
-        for item in r.json().get("items"):
+        for item in r.json():
             print(f"{item['sha']} {item['commit']['author']['name']}")
     except Exception as e:
         print(e)
