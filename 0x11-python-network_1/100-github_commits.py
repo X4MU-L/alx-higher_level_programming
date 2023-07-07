07a3be004a70575b3dba8c3ac77566756c733ff8
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-07 15:27:03 +0100

    (feat) -> change

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
     repo, username = argv[1:]
     url = f"https://api.github.com/repos/{username}/{repo}/commits?per_page=10&sort=committer-date"
     r = requests.get(url, auth=(
-        username, "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"))
+        username, "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"))
     try:
         for item in r.json():
-            print(f"{item['sha']} {item['commit']['author']['name']}")
+            print(
+                f"{item['sha']} {item['commit']['author']['name']}")
     except Exception as e:
         print(e)
         pass
