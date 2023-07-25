b4500be7c6031442521d11c2e5377c61a64a1a03
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-25 23:48:58 +0100

    (feat) -> parses and saves data to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,12 @@ const fs = require('fs');
 const url = `${process.argv[2]}`;
 request.get(url, (err, res, body) => {
   if (body) {
-    fs.writeFile(process.argv[3], body, 'utf-8', (err) => console.log(err));
+    fs.writeFile(
+      process.argv[3],
+      body,
+      'utf-8',
+      (err) => err && console.log(err)
+    );
   } else {
     console.log(err);
   }
