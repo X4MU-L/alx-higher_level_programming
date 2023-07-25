9b0bd2bafc90dc8d8d9351ef6e2fd47b00f83ad6
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-25 17:01:22 +0100

    (feat) -> consume an api and parse data using request

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const request = require('request');
+url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request.get(url, (err, res, body) => console.log(JSON.parse(body).title));
