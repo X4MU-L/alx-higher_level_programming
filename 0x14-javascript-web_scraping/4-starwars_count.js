d61805111d27cd5765d0e10ddb62f19e83324f6a
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-26 01:53:37 +0100

    (feat) -> refactor error printing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,13 @@ const request = require('request');
 const url = `${process.argv[2]}`;
 const person = 'https://swapi-api.alx-tools.com/api/people/18/';
 request.get(url, (err, res, body) => {
-  err && console.log(err);
-  let num = 0;
-  for (const film of JSON.parse(body).results) {
-    if (film.characters.find((usr) => usr === person)) {
-      num++;
+  if (!err) {
+    let num = 0;
+    for (const film of JSON.parse(body).results) {
+      if (film.characters.find((usr) => usr === person)) {
+        num++;
+      }
     }
+    console.log(num);
   }
-  console.log(num);
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,15 +2,18 @@
 const request = require('request');
 const url = `${process.argv[2]}`;
 request.get(url, (err, res, body) => {
-  err && console.log(err);
-  const obj = {};
-  const todos = JSON.parse(body);
-  for (const todo of todos) {
-    if (obj[todo.userId] && todo.completed) {
-      obj[todo.userId]++;
-    } else if (!obj[todo.userId] && todo.completed) {
-      obj[todo.userId] = 1;
+  if (!err) {
+    const obj = {};
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed) {
+        if (obj[todo.userId]) {
+          obj[todo.userId]++;
+        } else {
+          obj[todo.userId] = 1;
+        }
+      }
     }
+    console.log(obj);
   }
-  console.log(obj);
 });
