ab738e570719c1e78cbb31d0ef12f3fc0e48d3ca
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-08-02 23:17:21 +0100

    (feat) -> toggle class

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("DIV#toggle_header").click(function () {
+    if ($("header").attr("class") === "green") {
+      $("header").removeClass("green").addClass("red");
+    } else {
+      $("header").removeClass("red").addClass("green");
+    }
+  });
+});
